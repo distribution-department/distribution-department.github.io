d00ea55a5ea3ba98827560c7bbe3be6b1ec69fcb
Kibernetiki <evgenija.khamrik@gmail.com>
2026-06-04 19:58:31 +0300

    budget added

--- js/385.17b584ee.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkkiber_dashboard"]=self["webpackChunkkiber_dashboard"]||[]).push([[385],{3385:function(a,n,e){e.r(n),e.d(n,{default:function(){return d}});var r=e(6768);function t(a,n,e,t,u,s){const c=(0,r.g2)("ManagerPage");return(0,r.uX)(),(0,r.Wv)(c,{"manager-id":"artutjan"})}var u=e(4964),s={name:"ArtutjanPage",components:{ManagerPage:u.A}},c=e(1241);const o=(0,c.A)(s,[["render",t]]);var d=o}}]);
-//# sourceMappingURL=385.17b584ee.js.map
